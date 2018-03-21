61e58af22a08b0c1b4e6f03fc5198b50d9355e1a
Wonjun Jeong <nttejun@gmail.com>
2018-03-21 19:52:05 +0900

    Confirm sign up process

---
+++ sign-up/src/signup/SignUpCheckImpl.java
@@ -0,0 +1,9 @@
+package signup;
+
+// 회원가입
+public abstract class SignUpCheckImpl {
+    public abstract void checkUserId();
+    public abstract void checkUserPw();
+    public abstract void checkUserPhone();
+    public abstract void checkUserAgree();
+}
