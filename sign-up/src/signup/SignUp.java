006fd79d71967ef825c0d7653b00090aa6f23f72
Wonjun Jeong <nttejun@gmail.com>
2018-03-21 20:53:04 +0900

    Sign up logic and bridge

---
+++ sign-up/src/signup/SignUp.java
@@ -0,0 +1,22 @@
+package signup;
+
+public class SignUp {
+
+    private SignUpCheckImpl checkImpl;
+
+    public SignUp(SignUpCheckImpl checkImpl){
+        this.checkImpl = checkImpl;
+    }
+
+    public void checkId(){checkImpl.checkUserId();}
+    public void checkPw(){checkImpl.checkUserPw();}
+    public void checkPhone(){checkImpl.checkUserPhone();}
+    public void checkAgree(){checkImpl.checkUserAgree();}
+
+    public void checkStart(){
+        checkId();
+        checkPw();
+        checkPhone();
+        checkAgree();
+    }
+}
