2d3323905782d9821edb3f5d6990c36f4aa80456
nttejun <nttejun@gmail.com>
2019-08-13 01:31:16 +0900

    파일 복사 실패 테스트 케이스 생성

--- java/src/io/inputstream/Main.java
+++ java/src/io/inputstream/Main.java
@@ -2,13 +2,62 @@ package io.inputstream;
 
 import org.junit.Test;
 
+import java.io.*;
+import java.nio.file.Files;
+
 import static org.junit.Assert.assertEquals;
 
 public class Main {
 
     @Test
-    public void TEST_READ_FILE(){
-        assertEquals("test", "test");
+    public void TEST_CopyTxtFile() throws IOException {
+
+        File targetTest = new File("/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/targetTest.txt");
+        File target = new File("/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/target.txt");
+
+        InputStream inputStream = new FileInputStream(target);
+        OutputStream outputStream = new FileOutputStream(targetTest);
+
+        while (true){
+            int data = inputStream.read();
+            if(data == -1) break;
+            outputStream.write(data);
+        }
+
+        inputStream.close();
+        outputStream.close();
+
+
+        String sTargetTest = "/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/targetTest.txt";
+        String sTarget = "/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/target.txt";
+
+        byte[] bTargetTest = Files.readAllBytes(new File(sTargetTest).toPath());
+        byte[] bTarget = Files.readAllBytes(new File(sTarget).toPath());
+
+        assertEquals(bTargetTest, bTarget);
+
+    }
+
+    @Test
+    public void TEST_CopyImageFile() throws IOException {
+
+        File targetTest = new File("/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/before.png");
+        File target = new File("/Users/wjjeong/wjjeong/dev/project/java-code/java/resource/after.png");
+
+        InputStream inputStream = new FileInputStream(target);
+        OutputStream outputStream = new FileOutputStream(targetTest);
+
+        while (true){
+            int data = inputStream.read();
+            if(data == -1) break;
+            outputStream.write(data);
+        }
+
+        inputStream.close();
+        outputStream.close();
+
+        assertEquals(inputStream.toString(), outputStream.toString());
+
     }
 
 }
