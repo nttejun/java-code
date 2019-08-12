03ceec6c045cc402a24b67d0f25f3e6df8049f71
nttejun <nttejun@gmail.com>
2019-08-12 23:39:48 +0900

    Setting directory

---
+++ java/src/io/inputstream/Main.java
@@ -0,0 +1,13 @@
+package io.inputstream;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class Main {
+
+    @Test
+    void TEST_READ_FILE(){
+        assertEquals("test", "test");
+    }
+}
