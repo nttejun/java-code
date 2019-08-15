3542ac5c49f5e86d5387ce5997cd6ae1cb094e9b
nttejun <nttejun@gmail.com>
2019-08-15 09:40:01 +0900

    Added load xml to string

---
+++ test-taxInvoice/src/com.wjjeong.xml/SignedInfo.java
@@ -0,0 +1,112 @@
+package com.wjjeong.xml;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+
+public class SignedInfo {
+
+    @Test
+    public void TEST_LOAD_XML(){
+
+        try {
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            InputStream inputStream = new FileInputStream(new File("/Users/wjjeong/wjjeong/dev/project/java-code/test-taxInvoice/src/com/wjjeong/original.xml"));
+            Document document = documentBuilderFactory.newDocumentBuilder().parse(inputStream);
+
+            StringWriter stringWriter = new StringWriter();
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "false");
+            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+            String output = stringWriter.toString();
+            assertEquals(output, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><TaxInvoice xmlns=\"urn:kr:or:kec:standard:Tax:ReusableAggregateBusinessInformationEntitySchemaModule:1:0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:kr:or:kec:standard:Tax:ReusableAggregateBusinessInformationEntitySchemaModule:1:0 http://www.kec.or.kr/standard/Tax/TaxInvoiceSchemaModule_1.0.xsd\">\n" +
+                    "    <ExchangedDocument>\n" +
+                    "        <ID/>\n" +
+                    "        <IssueDateTime>20190814240000</IssueDateTime>\n" +
+                    "    </ExchangedDocument>\n" +
+                    "    <TaxInvoiceDocument>\n" +
+                    "        <IssueID>201908014100003350060463</IssueID>\n" +
+                    "        <TypeCode>0101</TypeCode>\n" +
+                    "        <DescriptionText/>\n" +
+                    "        <IssueDateTime>20190808</IssueDateTime>\n" +
+                    "        <PurposeCode>02</PurposeCode>\n" +
+                    "    </TaxInvoiceDocument>\n" +
+                    "    <TaxInvoiceTradeSettlement>\n" +
+                    "        <InvoicerParty>\n" +
+                    "            <ID>00000</ID>\n" +
+                    "            <TypeCode>TEST</TypeCode>\n" +
+                    "            <NameText>상호명</NameText>\n" +
+                    "            <ClassificationCode>업종명</ClassificationCode>\n" +
+                    "            <SpecifiedPerson>\n" +
+                    "                <NameText>대표자명</NameText>\n" +
+                    "            </SpecifiedPerson>\n" +
+                    "            <DefinedContact>\n" +
+                    "                <DepartmentNameText>TEST</DepartmentNameText>\n" +
+                    "                <PersonNameText>담당자명</PersonNameText>\n" +
+                    "                <TelephoneCommunication>000-000-0000</TelephoneCommunication>\n" +
+                    "                <URICommunication>TEST@TEST.co.kr</URICommunication>\n" +
+                    "            </DefinedContact>\n" +
+                    "            <SpecifiedAddress>\n" +
+                    "                <LineOneText>TEST주소</LineOneText>\n" +
+                    "            </SpecifiedAddress>\n" +
+                    "        </InvoicerParty>\n" +
+                    "        <InvoiceeParty>\n" +
+                    "            <ID>000000000</ID>\n" +
+                    "            <TypeCode>TEST</TypeCode>\n" +
+                    "            <NameText>CORPORATION</NameText>\n" +
+                    "            <ClassificationCode>CORPPART</ClassificationCode>\n" +
+                    "            <SpecifiedOrganization>\n" +
+                    "                <BusinessTypeCode>01</BusinessTypeCode>\n" +
+                    "            </SpecifiedOrganization>\n" +
+                    "            <SpecifiedPerson>\n" +
+                    "                <NameText>TESTER</NameText>\n" +
+                    "            </SpecifiedPerson>\n" +
+                    "            <PrimaryDefinedContact>\n" +
+                    "                <DepartmentNameText>test</DepartmentNameText>\n" +
+                    "                <PersonNameText>t100</PersonNameText>\n" +
+                    "                <TelephoneCommunication>00000000</TelephoneCommunication>\n" +
+                    "                <URICommunication>test@naver.com</URICommunication>\n" +
+                    "            </PrimaryDefinedContact>\n" +
+                    "            <SpecifiedAddress>\n" +
+                    "                <LineOneText>address</LineOneText>\n" +
+                    "            </SpecifiedAddress>\n" +
+                    "        </InvoiceeParty>\n" +
+                    "        <SpecifiedMonetarySummation>\n" +
+                    "            <ChargeTotalAmount>0000</ChargeTotalAmount>\n" +
+                    "            <TaxTotalAmount>000</TaxTotalAmount>\n" +
+                    "            <GrandTotalAmount>0000</GrandTotalAmount>\n" +
+                    "        </SpecifiedMonetarySummation>\n" +
+                    "    </TaxInvoiceTradeSettlement>\n" +
+                    "    <TaxInvoiceTradeLineItem>\n" +
+                    "        <SequenceNumeric>1</SequenceNumeric>\n" +
+                    "        <DescriptionText/>\n" +
+                    "        <InvoiceAmount>0000</InvoiceAmount>\n" +
+                    "        <ChargeableUnitQuantity>1</ChargeableUnitQuantity>\n" +
+                    "        <InformationText/>\n" +
+                    "        <NameText>PRODUCT</NameText>\n" +
+                    "        <PurchaseExpiryDateTime>000000</PurchaseExpiryDateTime>\n" +
+                    "        <TotalTax>\n" +
+                    "            <CalculatedAmount>000</CalculatedAmount>\n" +
+                    "        </TotalTax>\n" +
+                    "        <UnitPrice>\n" +
+                    "            <UnitAmount>0000</UnitAmount>\n" +
+                    "        </UnitPrice>\n" +
+                    "    </TaxInvoiceTradeLineItem>\n" +
+                    "</TaxInvoice>");
+
+        } catch (Exception e){ }
+    }
+}
