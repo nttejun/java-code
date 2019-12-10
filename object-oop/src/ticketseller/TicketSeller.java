376ed31e098659b59ec231c24bac9eda901d6453
nttejun <nttejun@gmail.com>
2019-12-10 11:28:49 +0900

    Modified theater enter role encapsulation

--- object-oop/src/ticketseller/TicketSeller.java
+++ object-oop/src/ticketseller/TicketSeller.java
@@ -1,5 +1,7 @@
 package ticketseller;
 
+import audience.Audience;
+import ticket.Ticket;
 import ticketoffice.TicketOffice;
 
 public class TicketSeller {
@@ -9,7 +11,15 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice(){
-        return ticketOffice;
+    public void sellTo(Audience audience){
+        if(audience.getBag().hasInvitation()){
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
